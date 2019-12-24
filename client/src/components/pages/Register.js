b2ef7cd189acdc2f01f8b62e1659b19d0107e784
GuRuGu <peter.k.nets@gmail.com>
2019-12-24 17:29:43 +0200

    Client --- style register form

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h1 className="login-title">Register User</h1>
+      <form className="form">
+        <div className="form-group">
+          <label className="form-label" htmlFor="name">
+            Name:
+          </label>
+          <input className="form-input" type="text" name="name"></input>
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">
+            Email:
+          </label>
+          <input className="form-input" type="email" name="email"></input>
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="password">
+            Password:
+          </label>
+          <input className="form-input" type="password" name="password"></input>
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="password2">
+            Confirm Password:
+          </label>
+          <input
+            className="form-input"
+            type="password"
+            name="password2"
+          ></input>
+        </div>
+        <button className="form-btn" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
