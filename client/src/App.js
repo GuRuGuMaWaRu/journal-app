71663242e77dfd9fd80cb7c64973c9795e48c97b
GuRuGu <peter.k.nets@gmail.com>
2019-12-24 14:00:38 +0200

    Client --- basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Main from "./components/pages/Main";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <nav>
+        <span>Journal App</span>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
